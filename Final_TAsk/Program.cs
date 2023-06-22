3d70a3c8f2e325f0fc4c39ae0ad3ca9e66a55e1a
Edem K <kimalidinov@list.ru>
2023-06-22 17:03:08 +0300

    Вызвали методы

--- Final_TAsk/Program.cs
+++ Final_TAsk/Program.cs
@@ -1,11 +1,11 @@
-﻿string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
-string[] array2 = new string[array1.Length];
+﻿string[] array_1 = new string[5] { "12355", "235463", "hello", "world", "res" };
+string[] array_2 = new string[array_1.Length];
 
 
 void CreateSecondArrayWithIf(string[] arr_1, string[] arr_2)
 {
     int count = 0;
-    for (int i = 0; i < array1.Length; i++)
+    for (int i = 0; i < arr_1.Length; i++)
     {
         if (arr_1[i].Length <= 3)
         {
@@ -17,9 +17,14 @@ void CreateSecondArrayWithIf(string[] arr_1, string[] arr_2)
 
 void PrintArray(string[] array)
 {
+    Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"{array[i]} ");
     }
+    Console.Write("]");
     Console.WriteLine();
 }
+
+CreateSecondArrayWithIf(array_1, array_2);
+PrintArray(array_2);
