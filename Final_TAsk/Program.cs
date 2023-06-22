42e936e9e20467a7ab6f9d83f11870dfb4613a94
Edem K <kimalidinov@list.ru>
2023-06-22 16:54:15 +0300

    Создали метод который заполняет массив лементами меньшими или равными 3 элементам

--- Final_TAsk/Program.cs
+++ Final_TAsk/Program.cs
@@ -1,2 +1,16 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+﻿string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
 string[] array2 = new string[array1.Length];
+
+
+void CreateSecondArrayWithIf(string[] arr_1, string[] arr_2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (arr_1[i].Length <= 3)
+        {
+            arr_2[count] = arr_1[i];
+            count++;
+        }
+    }
+}
